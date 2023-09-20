46ec86594d25907a56797b909b3d2bd4d3d9a235
farjana <farjanashaik153@gmail.com>
2023-09-20 23:10:10 +0530

    login and signup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  isLoginFormVisible: boolean = true;
 
+  constructor() { }
+
+  toggleForms(): void {
+    const loginForm = document.querySelector('.login-form') as HTMLElement;
+    const signupForm = document.querySelector('.signup-form') as HTMLElement;
+
+    if (this.isLoginFormVisible) {
+      // Move the login form slightly to the right (over the signup form)
+      loginForm.style.transform = 'translateX(10px)';
+      // Set a higher z-index to ensure login form is on top
+      loginForm.style.zIndex = '2';
+
+      // Reset the signup form position
+      signupForm.style.transform = 'translateX(0)';
+      signupForm.style.zIndex = '1';
+    } else {
+      // Reset the login form position
+      loginForm.style.transform = 'translateX(0)';
+      loginForm.style.zIndex = '1';
+
+      // Move the signup form slightly to the right (over the login form)
+      signupForm.style.transform = 'translateX(10px)';
+      // Set a higher z-index to ensure signup form is on top
+      signupForm.style.zIndex = '2';
+    }
+
+    this.isLoginFormVisible = !this.isLoginFormVisible;
+  }
 }
+
